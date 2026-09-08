a18ff0767324d5fb4da5c7c7c17ef265a74a7ea2
petrinagouvea-dot <petrina_gouvea@hotmail.com>
2026-09-08 10:16:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker para Estoque AP — Funcionalidade Offline
 
-const CACHE_NAME = 'estoque-ap-v2';
+const CACHE_NAME = 'estoque-ap-v3';
 const URLS_TO_CACHE = [
   './',
   './index.html',
